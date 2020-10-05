41507a223e4d4ab1be1a69b73a3ccedd312fbb89
Sarel <Sarelk@gmail.com>
2020-10-05 22:50:37 +0300

    Color picker in progress

--- pixel/src/components/Pixel.vue
+++ pixel/src/components/Pixel.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="pixel"></div>
+  <div :class="['pixel', color]"></div>
 </template>
 
 <script>
 export default {
   name: 'Pixel',
   props: {
-    msg: String
+    color: String
   }
 }
 </script>
 
 <style scoped>
   .pixel { border: 1px solid lightgray;width:30px;height:30px;box-sizing: border-box; }
+  .white {  background-color: white; }
+  .lightblue { background-color: rgb(0, 188, 212); }
+  .blue { background-color: rgb(3, 169, 244); }
+  .darkblue { background-color: rgb(33, 150, 243); }
 </style>
