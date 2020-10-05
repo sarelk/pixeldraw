13146e0ec1013b1b52962247e7a279daff512128
Sarel <Sarelk@gmail.com>
2020-10-05 22:43:57 +0300

    pixel grid

--- pixel/src/App.vue
+++ pixel/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Canvas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Canvas
   }
 }
 </script>
---
+++ pixel/src/components/Pixel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pixel"></div>
+</template>
+
+<script>
+export default {
+  name: 'Pixel',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  .pixel { border: 1px solid lightgray;width:30px;height:30px;box-sizing: border-box; }
+</style>
